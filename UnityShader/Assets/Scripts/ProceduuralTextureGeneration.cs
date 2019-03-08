5fd0a773e5ff5c925fba3f806a94480a3d6f97e7
王啸川 <m15048619182@163.com>
2019-03-09 00:00:58 +0800

    << 12

--- UnityShader/Assets/Scripts/ProceduuralTextureGeneration.cs
+++ UnityShader/Assets/Scripts/ProceduuralTextureGeneration.cs
@@ -1,57 +1,57 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-[ExecuteInEditMode]
-public class ProceduuralTextureGeneration : MonoBehaviour {
+// [ExecuteInEditMode]
+// public class ProceduuralTextureGeneration : MonoBehaviour {
 
-	public Material material = null;
+// 	public Material material = null;
 
-	#region Material properties
-	[SerializeField,SetProperty("textureWidth")]
-	private int m_textureWidth = 512;
-	public int textureWidth
-	{
-		get
-		{
-			return m_textureWidth;
-		}
-		set
-		{
-			m_textureWidth = value;
-			_UpdateMaterial();
-		}
-	}
+// 	#region Material properties
+// 	[SerializeField,SetProperty("textureWidth")]
+// 	private int m_textureWidth = 512;
+// 	public int textureWidth
+// 	{
+// 		get
+// 		{
+// 			return m_textureWidth;
+// 		}
+// 		set
+// 		{
+// 			m_textureWidth = value;
+// 			_UpdateMaterial();
+// 		}
+// 	}
 
-	[SerializeField,SetProperty("backgroundColor")]
-	private Color m_backgroundColor = Color.white;
-	public Color backgroundColor
-	{
-		get
-		{
-			return m_backgroundColor;
-		}
-		set
-		{
-			m_backgroundColor = value;
-			_UpdateMaterial();
-		}
-	}
+// 	[SerializeField,SetProperty("backgroundColor")]
+// 	private Color m_backgroundColor = Color.white;
+// 	public Color backgroundColor
+// 	{
+// 		get
+// 		{
+// 			return m_backgroundColor;
+// 		}
+// 		set
+// 		{
+// 			m_backgroundColor = value;
+// 			_UpdateMaterial();
+// 		}
+// 	}
 
-	[SerializeField,SetProperty("circleColor")]
-	private Color m_circleColor = Color.yellow;
-	public Color circleColor
-	{
-		get
-		{
-			return m_circleColor;
-		}
-		set
-		{
-			m_circleColor = value;
-			_UpdateMaterial();
-		}
-	}
+// 	[SerializeField,SetProperty("circleColor")]
+// 	private Color m_circleColor = Color.yellow;
+// 	public Color circleColor
+// 	{
+// 		get
+// 		{
+// 			return m_circleColor;
+// 		}
+// 		set
+// 		{
+// 			m_circleColor = value;
+// 			_UpdateMaterial();
+// 		}
+// 	}
 
-	#endregion
-}
+// 	#endregion
+// }
